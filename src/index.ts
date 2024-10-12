cbffcbaaf42dce550f9335d1616eb506cdd8d918
huynq <huynq.meditech@gmail.com>
2024-10-12 16:32:29 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import databaseService from '@/services/databaseService'
 import { defaultErrorHandler } from './middlewares/defaultErrorHandler'
 import usersRouter from './routes/usersRoutes'
 import authRouter from './routes/authRoutes'
-
+import cors from 'cors'
 const app = express()
 const port = 8080
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Cho phép yêu cầu từ origin này
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức HTTP mà bạn cho phép
+    credentials: true // Nếu bạn muốn cho phép cookies
+  })
+)
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/api', authRouter)
